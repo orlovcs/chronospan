61d6dd79ce2c7bc88a1357784b56b1c3b29688b3
Nikita Orlov <thetax45@gmail.com>
2025-01-02 16:18:09 -0500

    add useful constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,10 @@
 export const MINUTE_IN_SECONDS = 60000;
 export const HOUR_IN_SECONDS = 3600000;
 export const DAY_IN_SECONDS = 86400000;
+
+export const WEEK_IN_DAYS = 7;
+export const MONTH_IN_DAYS = 30;
+export const YEAR_IN_DAYS = 365;
+export const DECADE_IN_DAYS = 3650;
+export const CENTURY_IN_DAYS = 36500;
+export const MILLENNIUM_IN_DAYS = 365000;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MINUTE_IN_SECONDS } from "@constants";
+import { CENTURY_IN_DAYS, DAY_IN_SECONDS, DECADE_IN_DAYS, HOUR_IN_SECONDS, MILLENNIUM_IN_DAYS, MINUTE_IN_SECONDS, MONTH_IN_DAYS, WEEK_IN_DAYS, YEAR_IN_DAYS } from "@constants";
 import en from "@locales/en.json";
 import es from "@locales/es.json";
 import i18next from "i18next";
@@ -23,36 +23,35 @@ const timeAgo = (date?: Date | string, fuzzy?: boolean): string => {
   const now = new Date();
   const diff = now.getTime() - input.getTime();
   const diffInMinutes = Math.floor(diff / MINUTE_IN_SECONDS);
-  const diffInHours = Math.floor(diff / 3600000);
-  const diffInDays = Math.floor(diff / 86400000);
+  const diffInHours = Math.floor(diff / HOUR_IN_SECONDS);
+  const diffInDays = Math.floor(diff / DAY_IN_SECONDS);
 
   if (fuzzy) {
     if (diffInDays === 1) return i18next.t("yesterday");
-    if (diffInDays < 7) return i18next.t("lastWeek");
-    if (diffInDays < 30) return i18next.t("lastMonth");
-    if (diffInDays < 365) return i18next.t("lastYear");
-    if (diffInDays < 3650) return i18next.t("lastDecade");
-    if (diffInDays < 36500) return i18next.t("lastCentury");
+    if (diffInDays < WEEK_IN_DAYS) return i18next.t("lastWeek");
+    if (diffInDays < MONTH_IN_DAYS) return i18next.t("lastMonth");
+    if (diffInDays < YEAR_IN_DAYS) return i18next.t("lastYear");
+    if (diffInDays < DECADE_IN_DAYS) return i18next.t("lastDecade");
+    if (diffInDays < CENTURY_IN_DAYS) return i18next.t("lastCentury");
     return i18next.t("lastMillennium");
   }
-
   if (diffInMinutes < 1) return i18next.t("justNow");
-  if (diffInMinutes < 60)
+  if (diffInMinutes < HOUR_IN_SECONDS / MINUTE_IN_SECONDS)
     return i18next.t(
       diffInMinutes === 1 ? "minutesAgo.one" : "minutesAgo.other",
       { count: diffInMinutes }
     );
-  if (diffInHours < 24)
+  if (diffInHours < DAY_IN_SECONDS / HOUR_IN_SECONDS)
     return i18next.t(diffInHours === 1 ? "hoursAgo.one" : "hoursAgo.other", {
       count: diffInHours,
     });
-  if (diffInDays < 7)
+  if (diffInDays < WEEK_IN_DAYS)
     return i18next.t(diffInDays === 1 ? "daysAgo.one" : "daysAgo.other", {
       count: diffInDays,
     });
-  if (diffInDays < 30)
+  if (diffInDays < MONTH_IN_DAYS)
     return i18next.t(diffInDays === 1 ? "weeksAgo.one" : "weeksAgo.other", {
-      count: Math.floor(diffInDays / 7),
+      count: Math.floor(diffInDays / WEEK_IN_DAYS),
     });
 
   return input.toLocaleDateString();
@@ -70,69 +69,69 @@ const timeUntil = (date?: Date | string, fuzzy?: boolean): string => {
   const now = new Date();
   const diff = input.getTime() - now.getTime();
   const diffInMinutes = Math.floor(diff / MINUTE_IN_SECONDS);
-  const diffInHours = Math.floor(diff / 3600000);
-  const diffInDays = Math.floor(diff / 86400000);
+  const diffInHours = Math.floor(diff / HOUR_IN_SECONDS);
+  const diffInDays = Math.floor(diff / DAY_IN_SECONDS);
 
   if (fuzzy) {
     if (diffInDays === 1) return i18next.t("tomorrow");
-    if (diffInDays < 7) return i18next.t("nextWeek");
-    if (diffInDays < 30) return i18next.t("nextMonth");
-    if (diffInDays < 365) return i18next.t("nextYear");
-    if (diffInDays < 3650) return i18next.t("nextDecade");
-    if (diffInDays < 36500) return i18next.t("nextCentury");
+    if (diffInDays < WEEK_IN_DAYS) return i18next.t("nextWeek");
+    if (diffInDays < MONTH_IN_DAYS) return i18next.t("nextMonth");
+    if (diffInDays < YEAR_IN_DAYS) return i18next.t("nextYear");
+    if (diffInDays < DECADE_IN_DAYS) return i18next.t("nextDecade");
+    if (diffInDays < CENTURY_IN_DAYS) return i18next.t("nextCentury");
     return i18next.t("nextMillennium");
   }
 
   if (diffInMinutes < 1) return i18next.t("justNow");
-  if (diffInMinutes < 60)
+  if (diffInMinutes < HOUR_IN_SECONDS / MINUTE_IN_SECONDS)
     return i18next.t(
       diffInMinutes === 1 ? "minutesUntil.one" : "minutesUntil.other",
       { count: diffInMinutes }
     );
-  if (diffInHours < 24)
+  if (diffInHours < DAY_IN_SECONDS / HOUR_IN_SECONDS)
     return i18next.t(
       diffInHours === 1 ? "hoursUntil.one" : "hoursUntil.other",
       {
         count: diffInHours,
       }
     );
-  if (diffInDays < 7)
+  if (diffInDays < WEEK_IN_DAYS)
     return i18next.t(diffInDays === 1 ? "daysUntil.one" : "daysUntil.other", {
       count: diffInDays,
     });
-  if (diffInDays < 30)
+  if (diffInDays < MONTH_IN_DAYS)
     return i18next.t(diffInDays === 1 ? "weeksUntil.one" : "weeksUntil.other", {
-      count: Math.floor(diffInDays / 7),
+      count: Math.floor(diffInDays / WEEK_IN_DAYS),
     });
-  if (diffInDays < 365)
+  if (diffInDays < YEAR_IN_DAYS)
     return i18next.t(
       diffInDays === 1 ? "monthsUntil.one" : "monthsUntil.other",
       {
-        count: Math.floor(diffInDays / 30),
+        count: Math.floor(diffInDays / MONTH_IN_DAYS),
       }
     );
-  if (diffInDays < 3650)
+  if (diffInDays < DECADE_IN_DAYS)
     return i18next.t(diffInDays === 1 ? "yearsUntil.one" : "yearsUntil.other", {
-      count: Math.floor(diffInDays / 365),
+      count: Math.floor(diffInDays / YEAR_IN_DAYS),
     });
-  if (diffInDays < 36500)
+  if (diffInDays < CENTURY_IN_DAYS)
     return i18next.t(
       diffInDays === 1 ? "decadesUntil.one" : "decadesUntil.other",
       {
-        count: Math.floor(diffInDays / 3650),
+        count: Math.floor(diffInDays / DECADE_IN_DAYS),
       }
     );
-  if (diffInDays < 365000)
+  if (diffInDays < MILLENNIUM_IN_DAYS)
     return i18next.t(
       diffInDays === 1 ? "centuriesUntil.one" : "centuriesUntil.other",
       {
-        count: Math.floor(diffInDays / 36500),
+        count: Math.floor(diffInDays / CENTURY_IN_DAYS),
       }
     );
   return i18next.t(
     diffInDays === 1 ? "millenniaUntil.one" : "millenniaUntil.other",
     {
-      count: Math.floor(diffInDays / 365000),
+      count: Math.floor(diffInDays / MILLENNIUM_IN_DAYS),
     }
   );
 };
